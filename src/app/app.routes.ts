a5633ff097eb74656790c3c2d968ae6f842d81aa
Fatih Ozer <fatihozer246@gmail.com>
2024-07-09 09:56:36 +0200

    First commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { LayoutsComponent } from './components/layouts/layouts.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "login",
+    component: LoginComponent
+  },
+  {
+    path: "",
+    component: LayoutsComponent,
+    children: [
+      {
+        path: "",
+        component:HomeComponent
+      }
+    ]
+  }
+];
